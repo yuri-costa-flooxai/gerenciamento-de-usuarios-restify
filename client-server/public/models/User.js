9dcba3bbfe4a68058b0c8f567e3ac861ef6d4135
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-15 11:18:37 -0300

    feat: refactor for fetch api

--- client-server/public/models/User.js
+++ client-server/public/models/User.js
@@ -80,7 +80,7 @@ class User {
 
     static getUsersStorage() {
 
-        return HttpRequest.get('/users');
+        return Fetch.get('/users');
 
     }
 
@@ -106,11 +106,11 @@ class User {
 
             if (this.id) {
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON());
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON());
 
             } else {
 
-                promise = HttpRequest.post(`/users`, this.toJSON());
+                promise = Fetch.post(`/users`, this.toJSON());
 
             }
 
@@ -132,7 +132,7 @@ class User {
 
     remove(){
 
-        return HttpRequest.delete(`/users/${this.id}`);
+        return Fetch.delete(`/users/${this.id}`);
 
     }
 
