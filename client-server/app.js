e12f54cd86b0ed398875bfb857ec6a85b7938b8c
yuri-costa-flooxai <yuri.costa.flooxai@gmail.com>
2025-04-15 10:46:02 -0300

    feat: crud operations

--- client-server/app.js
+++ client-server/app.js
@@ -1,21 +1,41 @@
+var createError = require('http-errors');
 var express = require('express');
-var assert = require('assert');
-var restify = require('restify-clients');
-var router = express.Router();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var client = restify.createJsonClient({
-    url: 'http://localhost:4000'
-})
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+var app = express();
 
-    client.get('/users', function(err, request, response, obj) {
-        assert.ifError(err);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-        res.end(JSON.stringify(obj, null, 2));
-    });
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-module.exports = router;
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
